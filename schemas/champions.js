47104e54d1938baac67277502dc66a1376df7805
Lambert <lambertkuang@gmail.com>
2016-05-25 21:17:25 -0700

    Rename field

--- schemas/champions.js
+++ schemas/champions.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const championSchema = new Schema({
-  name: String,
   championId: Number,
+  championName: String,
   gamesWon: Number,
   gamesTotal: Number
 });
