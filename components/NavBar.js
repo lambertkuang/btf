6394ef3c0eceec294380b4f1703ab615812ad70d
Lambert <lambertkuang@gmail.com>
2016-06-13 18:33:00 -0700

    Add some styling to nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,11 +2,22 @@ import React from 'react';
 import NavLink from './NavLink';
 
 const navStyle = {
-  listStyle: 'none'
+  listStyle: 'none',
+  width: 100%,
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+  padding: 'initial',
+  display: 'flex',
+  alignItems: 'center',
+  background: 'white'
 };
 
 const listStyle = {
-  display: 'inline'
+  display: 'inline-block',
+  width: 33%
 };
 
 export default class NavBar extends React.Component {
--- components/NavLink.js
+++ components/NavLink.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+const linkStyle = {
+  textDecoration: 'none'
+};
+
 export default class NavLink extends React.Component {
   render() {
     return (
-      <Link {...this.props} activeClassName='active'/>
+      <Link {...this.props} style={linkStyle} activeClassName='active'/>
     );
   }
 }
