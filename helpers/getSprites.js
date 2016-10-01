558cc30768e96a5726b532c82d62704a51c3dc6e
Lambert <lambertkuang@gmail.com>
2016-10-01 13:23:27 -0700

    Stream latest image sprites

--- helpers/getSprites.js
+++ helpers/getSprites.js
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import request from 'request';
+import path from 'path';
 
+const API_KEY = fs.readFileSync(path.join(__dirname, '../private/api_key.txt')).toString();
 const url = 'http://ddragon.leagueoflegends.com/cdn/6.12.1/img/sprite/';
+const versionUrl = `https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key=${API_KEY}`;
 const sheets = [
   'champion0.png',
   'champion1.png',
@@ -10,12 +13,17 @@ const sheets = [
   'champion4.png'
 ];
 
-sheets.forEach((sheet) => {
-  request(url + sheet, {encoding: 'binary'}, (err, res, body) => {
-    fs.writeFile('public/images/' + sheet, body, 'binary', (err) => {
-      if (err) {
-        throw err;
-      }
+request.get(versionUrl, function(err, res, body) {
+  if (err) {
+    console.log('Error getting version: ', err);
+    return;
+  }
+  if (body) {
+    let version = JSON.parse(body).shift();
+    let url = `http://ddragon.leagueoflegends.com/cdn/${version}/img/sprite/`;
+
+    sheets.forEach((sheet) => {
+      request(url + sheet).pipe(fs.createWriteStream(path.join('public/images', sheet)));
     });
-  })
+  }
 });
